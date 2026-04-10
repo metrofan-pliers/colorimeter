85f0e4d58a08679d0092689143629b42728f0d2b
metrofan-pliers <igleb0260@gmail.com>
2026-04-10 20:11:16 +0300

    update run_server.bat

--- run_server.bat
+++ run_server.bat
@@ -1,5 +1,4 @@
 @echo off
-cd "C:\Users\igleb\Documents\python"
-call venv\Scripts\activate.bat
-python -m uvicorn server:app --host 0.0.0.0 --port 8000
+cd /d "C:\Users\igleb\Documents\git\colorimeter"
+python server.py
 pause
